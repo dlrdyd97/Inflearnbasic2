349368f5182c76b96296873e5b2ca6cc63e01c8c
dlrdyd97 <dlrdyd97@naver.com>
2023-11-12 22:26:36 +0900

    MemberApp 테스트

---
+++ happy/src/main/java/hello/happy/MemberApp.java
@@ -0,0 +1,21 @@
+package hello.happy;
+
+import hello.happy.member.Grade;
+import hello.happy.member.Member;
+import hello.happy.member.MemberService;
+import hello.happy.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+
+        MemberService memberService=new MemberServiceImpl();
+        Member kim = new Member(1L, "Kim", Grade.VIP);
+        memberService.join(kim);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member = "+findMember.getName());
+        System.out.println("find Member = "+kim.getName());
+
+    }
+}
