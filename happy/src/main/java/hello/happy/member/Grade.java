e4b52ee1172dea0af4a17029bdf0c70becceb341
dlrdyd97 <dlrdyd97@naver.com>
2023-11-12 21:59:32 +0900

    회원 도메인 개발

---
+++ happy/src/main/java/hello/happy/member/Grade.java
@@ -0,0 +1,6 @@
+package hello.happy.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ happy/src/main/java/hello/happy/member/Member.java
@@ -0,0 +1,39 @@
+package hello.happy.member;
+
+public class Member {
+
+    private Long id;
+    // Why Long? Wrapper Class 사용,null값을 사용할 수 있기 때문, Integer보다 표현범위가 넓기 때문
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+}
