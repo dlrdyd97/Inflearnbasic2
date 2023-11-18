2bc23220dac3c96725373d5a0dcc822e51bd4962
dlrdyd97 <dlrdyd97@naver.com>
2023-11-18 16:57:11 +0900

    AppConfig 리팩토링

--- happy/src/main/java/hello/happy/AppConfig.java
+++ happy/src/main/java/hello/happy/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.happy;
 
+import hello.happy.discount.DiscountPolicy;
 import hello.happy.discount.FixDiscountPolicy;
 import hello.happy.discount.RateDiscountPolicy;
 import hello.happy.member.MemberService;
@@ -11,13 +12,20 @@ import hello.happy.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){ // 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 
 
 }
