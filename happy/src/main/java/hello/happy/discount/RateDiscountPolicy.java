b26e5da118b00c7052092e00c722c6ab9f95e01a
dlrdyd97 <dlrdyd97@naver.com>
2023-11-30 22:05:51 +0900

    @Autowired 필드 명, @Qualifier, @Primary

--- happy/src/main/java/hello/happy/discount/RateDiscountPolicy.java
+++ happy/src/main/java/hello/happy/discount/RateDiscountPolicy.java
@@ -2,9 +2,12 @@ package hello.happy.discount;
 
 import hello.happy.member.Grade;
 import hello.happy.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
--- happy/src/main/java/hello/happy/order/OrderServiceImpl.java
+++ happy/src/main/java/hello/happy/order/OrderServiceImpl.java
@@ -6,12 +6,13 @@ import hello.happy.member.Member;
 import hello.happy.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor // 생성자코드를 그대로 똑같이 만들어줌
 // final 붙은값을 파라미터로 받는 생성자를 만들어준다
-public class OrderServiceImpl /* extends Object */ implements OrderService {
+public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
